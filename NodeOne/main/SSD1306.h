0de18358a1b81c2a935d0ab86c4a2a7f9abcc9a3
thanhvodong <thanhvd.ce@gmail.com>
2024-07-25 15:10:40 +0700

    Node 1 of Smart Home project

---
+++ NodeOne/main/SSD1306.h
@@ -0,0 +1,59 @@
+
+#ifndef MAIN_SSD1366_H_
+#define MAIN_SSD1366_H_
+
+// Following definitions are bollowed from 
+// http://robotcantalk.blogspot.com/2015/03/interfacing-arduino-with-ssd1306-driven.html
+
+// SLA (0x3C) + WRITE_MODE (0x00) =  0x78 (0b01111000)
+#define OLED_I2C_ADDRESS   0x3C
+
+// Control byte
+#define OLED_CONTROL_BYTE_CMD_SINGLE    0x80
+#define OLED_CONTROL_BYTE_CMD_STREAM    0x00
+#define OLED_CONTROL_BYTE_DATA_STREAM   0x40
+
+// Fundamental commands (pg.28)
+#define OLED_CMD_SET_CONTRAST           0x81    // follow with 0x7F
+#define OLED_CMD_DISPLAY_RAM            0xA4
+#define OLED_CMD_DISPLAY_ALLON          0xA5
+#define OLED_CMD_DISPLAY_NORMAL         0xA6
+#define OLED_CMD_DISPLAY_INVERTED       0xA7
+#define OLED_CMD_DISPLAY_OFF            0xAE
+#define OLED_CMD_DISPLAY_ON             0xAF
+
+// Addressing Command Table (pg.30)
+#define OLED_CMD_SET_MEMORY_ADDR_MODE   0x20
+#define OLED_CMD_SET_HORI_ADDR_MODE     0x00    // Horizontal Addressing Mode
+#define OLED_CMD_SET_VERT_ADDR_MODE     0x01    // Vertical Addressing Mode
+#define OLED_CMD_SET_PAGE_ADDR_MODE     0x02    // Page Addressing Mode
+#define OLED_CMD_SET_COLUMN_RANGE       0x21    // can be used only in HORZ/VERT mode - follow with 0x00 and 0x7F = COL127
+#define OLED_CMD_SET_PAGE_RANGE         0x22    // can be used only in HORZ/VERT mode - follow with 0x00 and 0x07 = PAGE7
+
+// Hardware Config (pg.31)
+#define OLED_CMD_SET_DISPLAY_START_LINE 0x40
+#define OLED_CMD_SET_SEGMENT_REMAP_0    0xA0    // normal
+#define OLED_CMD_SET_SEGMENT_REMAP_1    0xA1    // reverse
+#define OLED_CMD_SET_MUX_RATIO          0xA8    // follow with 0x3F = 64 MUX
+#define OLED_CMD_SET_COM_SCAN_MODE_0    0xC8    // top -> bottom
+#define OLED_CMD_SET_COM_SCAN_MODE_1    0xC8    // bottom -> top
+#define OLED_CMD_SET_DISPLAY_OFFSET     0xD3    // follow with 0x00
+#define OLED_CMD_SET_COM_PIN_MAP        0xDA    // follow with 0x12
+#define OLED_CMD_NOP                    0xE3    // NOP
+
+// Timing and Driving Scheme (pg.32)
+#define OLED_CMD_SET_DISPLAY_CLK_DIV    0xD5    // follow with 0x80
+#define OLED_CMD_SET_PRECHARGE          0xD9    // follow with 0xF1
+#define OLED_CMD_SET_VCOMH_DESELCT      0xDB    // follow with 0x30
+
+// Charge Pump (pg.62)
+#define OLED_CMD_SET_CHARGE_PUMP        0x8D    // follow with 0x14
+
+#define OLED_CMD_HORIZONTAL_RIGHT_SCROLL    0x26
+#define OLED_CMD_HORIZONTAL_LEFT_SCROLL     0x27
+#define OLED_CMD_TIME_INTERVAL_5_FRAMES     0X00
+#define OLED_CMD_TIME_INTERVAL_5_FRAMES     0X00
+#define OLED_CMD_DEACTIVE_SCROLL            0x2E
+#define OLED_CMD_ACTIVATE_SCROLL            0x2F
+
+#endif /* MAIN_SSD1366_H_ */
--- NodeOne/main/app_main.c
+++ NodeOne/main/app_main.c
@@ -1,3 +1,4 @@
+
 /**************DEFAULT CONFIGURATION*****************/
 #include <stdio.h>
 #include <stdint.h>
@@ -36,11 +37,40 @@
 #include "freertos/event_groups.h"
 #include "freertos/semphr.h"
 #include "freertos/queue.h"
-/****************************************************/
 
+//SSD includes
+#include "nvs.h"
+#include "cJSON.h"
+#include "driver/i2c.h"
+#include "SSD1306.h"
+#include "font.h"
+
+
+/****************************************************/
+/*DEFINE CHO MÀN HÌNH*/
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+
+#define DATA_LENGTH 512                  /*!< Data buffer length of test buffer */
+#define RW_TEST_LENGTH 128               /*!< Data length for r/w test, [0,DATA_LENGTH] */
+#define DELAY_TIME_BETWEEN_ITEMS_MS 1000 /*!< delay time between different test items */
+
+#define I2C_MASTER_SCL_IO GPIO_NUM_22 /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO GPIO_NUM_21 /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUMBER(0)  /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0   /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0   /*!< I2C master doesn't need buffer */
+
+#define WRITE_BIT I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ   /*!< I2C master read */
+#define ACK_CHECK_EN 0x1           /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0          /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                /*!< I2C ack value */
+#define NACK_VAL 0x1               /*!< I2C nack value */
 /****************CUSTOM DEFINES**********************/
 // MQRTT Broker URL
-#define MQTT_BROKER_URL "mqtts://aadd81fb96c3478d9cf64cf7a57edd8e.s1.eu.hivemq.cloud"
+#define MQTT_BROKER_URL "mqtts://c0116143ef0143149b4c43981ffead03.s1.eu.hivemq.cloud"
 // Fan configuration
 #define ENA_PIN GPIO_NUM_14
 #define IN1_PIN GPIO_NUM_27
@@ -60,7 +90,7 @@ char Current_Date_Time[100];
 // MQTT Credentials
 static const char *TAG = "mqtts_example";
 static const char username[] = "CE232";
-static const char password[] = "Thanh0512";
+static const char password[] = "Nhi09032003";
 esp_mqtt_client_handle_t client;
 // FAN Topic
 char *fan_topic = "smarthome/fan/control";
@@ -77,8 +107,85 @@ char MQTT_BUFFER[MQTT_sizeoff];
 char Str_ND[MQTT_sizeoff];
 char Str_DA[MQTT_sizeoff];
 char str[MQTT_sizeoff];
+char strtemp[50];
+char strhum[50];
 bool mqtt_connect_status = false;
+
+//SSD
+#define ESP_ERR (__STATUX__) if ()
+SemaphoreHandle_t print_mux = NULL;
+static const char *TAG3 = "OLED";
 /****************************************************/
+void ssd1306_init();
+void LCD();
+void task_ssd1306_display_text(const void *arg_text, uint8_t _page, uint8_t _seg);
+void task_ssd1306_display_clear();
+esp_err_t task_ssd1306_display_location(uint8_t _page, uint8_t _seg);
+esp_err_t task_ssd1306_display_image(uint8_t *images, uint8_t _page, uint8_t _seg, int _size);
+
+/*Khai báo các function của Quạt*/
+void time_sync_notification_cb(struct timeval *tv);
+static void sntp_setup(void);
+void set_systemtime_sntp();
+void fan_control(int speed);
+void fan_off_callback(void *arg);
+void start_fan_timer(int64_t duration_us);
+void check_time_and_turn_on_fan(int start_hour, int start_minute, int start_second, int duration_hour, int duration_minute, int duration_second, int fan_speed);
+void monitor_and_control_fan(int start_hour, int start_minute, int start_second, int duration_hour, int duration_minute, int duration_second, int fan_speed);
+void publish_fan_speed_message(int speed);
+static void app_handle_mqtt_data(esp_mqtt_event_handle_t event);
+
+/**************************CONFIG CHO MÀN HÌNH***********************************/
+static esp_err_t __attribute__((unused)) i2c_master_read_slave(i2c_port_t i2c_num, uint8_t *data_rd, size_t size)
+{
+    if (size == 0)
+    {
+        return ESP_OK;
+    }
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | READ_BIT, ACK_CHECK_EN);
+    if (size > 1)
+    {
+        i2c_master_read(cmd, data_rd, size - 1, ACK_VAL);
+    }
+    i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+static esp_err_t __attribute__((unused)) i2c_master_write_slave(i2c_port_t i2c_num, uint8_t *data_wr, size_t size)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 /portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+        // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
+    };
+    esp_err_t err = i2c_param_config(i2c_master_port, &conf);
+    if (err != ESP_OK)
+    {
+        return err;
+    }
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
 
 /*************GET REAL TIME FUNCTIONS****************/
 void time_sync_notification_cb(struct timeval *tv)
@@ -204,14 +311,7 @@ void check_time_and_turn_on_fan(int start_hour, int start_minute, int start_seco
         start_fan_timer(duration_us);
     }
 }
-// void monitor_and_control_fan(int start_hour, int start_minute, int start_second, int duration_hour, int duration_minute, int duration_second, int fan_speed)
-// {
-//     while (!fan_state) 
-//     {
-//         check_time_and_turn_on_fan(start_hour, start_minute, start_second, duration_hour, duration_minute, duration_second, fan_speed);
-//         usleep(1000000);
-//     }
-// }
+
 void monitor_and_control_fan_task(void *params)
 {
     int *args = (int *)params;
@@ -318,41 +418,160 @@ void delay(uint32_t time)
 {
     vTaskDelay(time / portTICK_PERIOD_MS);
 }
-// void dht11_task(){
-//     LastSendMQTT = esp_timer_get_time() / 1000;
-//     while (1)
-//     {
-//         if (esp_timer_get_time() / 1000 - LastSendMQTT >= 1500)
-//         {
-//             if (mqtt_connect_status )
-//             {
-//                 MQTT_DataJson();
-//                 esp_mqtt_client_publish(mqtt_client, dht_topic, MQTT_BUFFER, 0, 1, 0);
-//                 ESP_LOGI(TAG2, "SEND MQTT %s", MQTT_BUFFER);
-//                 delay(1000);
-//                 taskYIELD();
-//             }
-//             LastSendMQTT = esp_timer_get_time() / 1000;
-//         }
-//         delay(50);
-//     }
-//     vTaskDelete(NULL);
-// }
+
 void read_sensor_task(void *params)
-{
+{   
+    DHT11_init(DHT11_PIN);
     while (1)
     {
-        DHT11_init(DHT11_PIN);
-        // int temperature = DHT11_read().temperature;
-        // int humidity = DHT11_read().humidity;
-        // ESP_LOGI(TAG, "Temperature: %d C, Humidity: %d %%", temperature, humidity);
-        // snprintf(MQTT_BUFFER, MQTT_sizeoff, "{\"temperature\": %d, \"humidity\": %d}", temperature, humidity);
-        MQTT_DataJson();
-        esp_mqtt_client_publish(client, dht_topic, MQTT_BUFFER, 0, 1, 0);
-        ESP_LOGI(TAG2, "SEND MQTT %s", MQTT_BUFFER);
-        vTaskDelay(10000 / portTICK_PERIOD_MS); // Delay for 1 minute
+        if(DHT11_read().temperature > 0 && DHT11_read().humidity > 0)
+        {
+            
+            char str[500];
+            task_ssd1306_display_clear();
+            sprintf (strtemp, "nhiet do: %d", DHT11_read().temperature);
+            task_ssd1306_display_text(strtemp, 2, 32);
+            sprintf (strhum, "do am: %d", DHT11_read().humidity);
+            task_ssd1306_display_text(strhum, 4, 32);
+            delay(50);
+            MQTT_DataJson();
+            esp_mqtt_client_publish(client, dht_topic, MQTT_BUFFER, 0, 1, 0);
+            ESP_LOGI(TAG2, "SEND MQTT %s", MQTT_BUFFER);
+            vTaskDelay(10000 / portTICK_PERIOD_MS); // Delay for 1 minute
+        }
     }
     vTaskDelete(NULL);
+}
+void ssd1306_init()
+{
+    esp_err_t espRc;
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+
+    i2c_master_write_byte(cmd, OLED_CMD_SET_MEMORY_ADDR_MODE, true);
+    i2c_master_write_byte(cmd, OLED_CMD_SET_PAGE_ADDR_MODE, true);
+    // set lower and upper column register address 0b upper = 0000, lower 0000,
+    i2c_master_write_byte(cmd, 0x00, true);
+    i2c_master_write_byte(cmd, 0x10, true);
+
+    i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
+    i2c_master_write_byte(cmd, 0x14, true);
+
+    i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP_1, true); // reverse left-right mapping
+    i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE_0, true); // reverse up-bottom mapping
+
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_OFF, true);
+    i2c_master_write_byte(cmd, OLED_CMD_DEACTIVE_SCROLL, true); // 2E
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_NORMAL, true);  // A6
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);      // AF
+
+    i2c_master_stop(cmd);
+
+    espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+    if (espRc == ESP_OK)
+    {
+        ESP_LOGI(TAG3, "OLED configured successfully");
+    }
+    else
+    {
+        ESP_LOGE(TAG3, "OLED configuration failed. code: 0x%.2X", espRc);
+    }
+    i2c_cmd_link_delete(cmd);
+    return;
+}
+
+void task_ssd1306_display_text(const void *arg_text, uint8_t _page, uint8_t _seg)
+{
+    char *text = (char *)arg_text;
+    uint8_t text_len = strlen(text);
+
+    uint8_t image[8];
+
+    if (task_ssd1306_display_location(_page, _seg) == ESP_OK)
+    {
+        for (uint8_t i = 0; i < text_len; i++)
+        {
+            memcpy(image,font8x8_basic_tr[(uint8_t)text[i]],8);
+            task_ssd1306_display_image(image, _page, _seg,sizeof(image));
+            _seg = _seg + 8;
+        }
+    }
+    return;
+}
+esp_err_t task_ssd1306_display_location(uint8_t _page, uint8_t _seg)
+{
+    i2c_cmd_handle_t cmd;
+
+    esp_err_t status = 0;
+
+    uint8_t lowColumnSeg = _seg & 0x0F;
+    uint8_t highColumnSeg = (_seg >> 4) & 0x0F;
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+    i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+    i2c_master_write_byte(cmd, 0x00 | lowColumnSeg, true);  // reset column - choose column --> 0
+    i2c_master_write_byte(cmd, 0x10 | highColumnSeg, true); // reset line - choose line --> 0
+    i2c_master_write_byte(cmd, 0xB0 | _page, true);         // reset page
+
+    i2c_master_stop(cmd);
+    
+    status = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+    if (status == ESP_OK)
+    {
+        
+        return status;
+    }
+    else
+    {
+    }
+    i2c_cmd_link_delete(cmd);
+    return status;
+}
+esp_err_t task_ssd1306_display_image(uint8_t *images, uint8_t _page, uint8_t _seg, int _size)
+{
+    // ESP_LOGI(TAG, "Size : %d" , _size);
+    esp_err_t status = 0;
+
+    i2c_cmd_handle_t cmd;
+    
+    if (task_ssd1306_display_location(_page, _seg) == ESP_OK)
+    {
+        cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+        i2c_master_write(cmd, images , _size, true);
+
+        i2c_master_stop(cmd);
+        status = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
+
+    }
+    return status;
+}
+void task_ssd1306_display_clear()
+{
+    esp_err_t status ;
+
+    uint8_t clear[128];
+    for (uint8_t i = 0; i < 128; i++)
+    {
+        clear[i] = 0x00;
+    }
+
+    for (uint8_t _page = 0; _page < 8; _page++)
+    {
+        status = task_ssd1306_display_image(clear, _page , 0x00 , sizeof(clear));  
+    }
+    return;
 }
 /****************************************************/
 #if CONFIG_BROKER_CERTIFICATE_OVERRIDDEN == 1
@@ -439,24 +658,7 @@ void mqtt_app_start(void)
     /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
-    // LastSendMQTT = esp_timer_get_time() / 1000;
-    // while (1)
-    // {
-    //     if (esp_timer_get_time() / 1000 - LastSendMQTT >= 1500)
-    //     {
-    //         if (mqtt_connect_status )
-    //         {
-    //             MQTT_DataJson();
-    //             esp_mqtt_client_publish(mqtt_client, dht_topic, MQTT_BUFFER, 0, 1, 0);
-    //             ESP_LOGI(TAG2, "SEND MQTT %s", MQTT_BUFFER);
-    //             delay(1000);
-    //             taskYIELD();
-    //         }
-    //         LastSendMQTT = esp_timer_get_time() / 1000;
-    //     }
-    //     delay(50);
-    // }
-    // vTaskDelete(NULL);
+
 }
 void app_main(void)
 {
@@ -480,6 +682,9 @@ void app_main(void)
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
      * examples/protocols/README.md for more information about this function.
      */
+    print_mux = xSemaphoreCreateMutex();
+    ESP_ERROR_CHECK(i2c_master_init());
+    ssd1306_init();
     DHT11_init(DHT11_PIN);
     ESP_ERROR_CHECK(example_connect());
     set_systemtime_sntp();
